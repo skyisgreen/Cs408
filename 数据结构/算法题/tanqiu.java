fa7a27410d381b3103739564d6b989d58fed17a2
EasyZhang <35111939+skyisgreen@users.noreply.github.com>
2019-01-15 23:33:53 +0800

    Create tanqiu.java

---
+++ 数据结构/算法题/tanqiu.java
@@ -0,0 +1,16 @@
+//球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，
+// 求它在第10次落地时，共经过多少米？第10次反弹多高？
+public class tanqiu {
+    public static void main(String[] args)
+    {
+        double s=0;
+        double t=100;
+        for(int i=1;i<=10;i++)
+        {
+            s+=t;
+            t=t/2;
+        }
+        System.out.println(s);
+        System.out.println(t);
+    }
+}
