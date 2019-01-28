3e1544245134bf0b7ab52ccc328eb4e2970af5dc
EasyZhang <35111939+skyisgreen@users.noreply.github.com>
2019-01-28 23:41:50 +0800

    Create Rollingdivision.java

---
+++ 数据结构/算法题/Rollingdivision.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//题目：输入两个正整数m和n，求其最大公约数和最小公倍数。
+//1.程序分析：利用辗除法。
+public class Rollingdivision {
+    public static int gcd(int m, int n)
+    {
+        while(true)
+        {
+            if((m=m%n)==0)
+                return n;
+            if((n=n%m)==0)
+                return m;
+        }
+    }
+    public static void main(String arg[]) throws Exception
+    {
+        Scanner chin = new Scanner(System.in);
+        int a =chin.nextInt();
+        int b =chin.nextInt();
+        int c =gcd(a,b);
+        System.out.println("最小公倍数："+a*b/c+"\n最大公约数："+c);
+    }
+
+}
