29d6218cf68f1fe6600aa737cb1b8ec49692a91b
EasyZhang <35111939+skyisgreen@users.noreply.github.com>
2019-02-11 23:00:26 +0800

    Create doublenine.java

---
+++ 数据结构/算法题/doublenine.java
@@ -0,0 +1,13 @@
+public class doublenine {
+    public static void main(String[] args)
+    {
+        int i=0;
+        int j=0;
+        for(i=1;i<=9;i++)
+        {	for(j=1;j<=9;j++)
+            System.out.print(i+"*"+j+"="+i*j+"\t");
+            System.out.println();
+        }
+    }
+
+}
