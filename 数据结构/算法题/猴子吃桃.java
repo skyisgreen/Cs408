a94c5c42de0c785470996a1bbe596a00f95d526e
EasyZhang <35111939+skyisgreen@users.noreply.github.com>
2019-02-12 22:50:24 +0800

    Create 猴子吃桃.java

---
+++ 数据结构/算法题/猴子吃桃.java
@@ -0,0 +1,16 @@
+public class 猴子吃桃 {
+    static int total(int day)
+    {
+        if(day==10)
+        {
+            return 1;
+        }
+        else{
+            return (total(day+1)+1)*2;
+        }
+    }
+public static void main(String[] args)
+{
+    System.out.println(total(1));
+}
+}
